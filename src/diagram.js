c8c84ca1fb8ce9507dc64827c4dd7f7e1696e661
Frederik Leidloff <fleidloff@team.mobile.de>
2016-01-04 16:08:21 +0100

    remove duplicate probabilities

--- src/diagram.js
+++ src/diagram.js
@@ -11,11 +11,17 @@ function expectedDate(workingDays, input) {
 
 var diagram = function(resultDays, input) {
 	var total = 0;
+	var lastProbability = 0;
 	return Object.keys(resultDays)
 		.sort()
 		.map(function(item) {
 			total += resultDays[item]
 			return { workingDays: item, probability: Math.ceil(total / input.cycles * 100), date: expectedDate(item, input)};
+		})
+		.filter(function(item) {
+			var unique = lastProbability !== item.probability;
+			lastProbability = item.probability;
+			return unique;
 		});
 }
 
